6c7f87e54c807d47c743fdbb01ad06cb66533260
Woo Jia Hao <woojiahao1234@gmail.com>
2024-02-13 10:03:04 +0800

    complete base static application

--- lib/practical_elixir_demo_web/controllers/page_html.ex
+++ lib/practical_elixir_demo_web/controllers/page_html.ex
@@ -5,16 +5,30 @@ defmodule PracticalElixirDemoWeb.PageHTML do
 
   def todo_item(assigns) do
     ~H"""
-    <div class="flex gap-x-4 mb-4 last:mb-0">
+    <div class="flex gap-x-4 mb-4 last:mb-0 items-center">
       <%= if @item.is_done? do %>
         <p>✅</p>
       <% else %>
         <p>❌</p>
       <% end %>
-      <div>
-        <p>
-          <%= @item.title %>
-        </p>
+      <div class="w-full">
+        <div class="flex justify-between items-center w-full">
+          <p>
+            <%= @item.title %>
+          </p>
+
+          <div>
+            <%= if @item.is_done? do %>
+              <button class="bg-blue-300 px-3 py-1 font-bold rounded-md text-sm">Mark as Done</button>
+            <% else %>
+              <button class="bg-green-300 px-3 py-1 font-bold rounded-md text-sm">
+                Mark as Not Done
+              </button>
+            <% end %>
+            <button class="bg-yellow-300 px-3 py-1 font-bold rounded-md text-sm">Edit</button>
+            <button class="bg-red-300 px-3 py-1 font-bold rounded-md text-sm">Delete</button>
+          </div>
+        </div>
         <p class="italic">
           <%= @item.description %>
         </p>
